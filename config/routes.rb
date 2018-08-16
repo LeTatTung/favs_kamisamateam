944be87e804823192ccefcf32854fbc8277eb0b6
katsukirei <al17035@shibaura-it.ac.jp>
2018-08-16 08:55:30 +0000

    image comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: :registrations,
     omniauth_callbacks: :omniauth_callbacks}
-
+    
+  resources :image_comments
+  resources :images
+  
   root "pages#show", page: "home"
   get "pages/:page", to: "pages#show", as: "page"
   resources :products, only: [:index, :show]
